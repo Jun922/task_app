48e9f8437e46300219edd4ad8f8ec0395bd6a1d5
Jun922 <junjunchi620@gmail.com>
2021-08-30 23:18:16 +0900

    error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    User.find(params[:id]).destroy
     if current_user.destroy(user_params)
       redirect_to root_path
     else
